e52b4e783723492ef123751ca2b09bdd3829c2e1
Ajay Sagar <ajaysudam.srinivasa@gmail.com>
2024-07-02 18:18:19 +0530

    drawer_ui_done

--- lib/shared/widgets/common_drawer_widget.dart
+++ lib/shared/widgets/common_drawer_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:srinivasa_crm_new/src/common/common.dart';
+import 'package:srinivasa_crm_new/src/config/constants/app_colors.dart';
 
 
 class CommonDrawerWidget extends StatelessWidget {
@@ -6,6 +9,49 @@ class CommonDrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+           Container(
+            padding: EdgeInsets.only(top: 60.h,left: 10.w),
+            decoration: const BoxDecoration(
+              color: AppColors.primaryColor,
+            ),
+            alignment: Alignment.centerLeft,
+            child:    const Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.person,color: AppColors.primaryColor,),
+                ),
+                Expanded(
+                  child: ListTile(
+                  title: CommonTextWidget(title: 'Ajay Sagar',textColor: Colors.white,),
+                  subtitle: CommonTextWidget(title: 'ajay@gmail.com',textColor: Colors.white,),
+                            ),
+                ),
+              ],
+            )
+          ),
+      
+         ListTile(
+  title: const Text('Item 1'),
+  onTap: () {
+    if (context.mounted) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pop(context);
+      });
+    }
+  },
+),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
